01af05deeaff2a9a5fc7cce1510678990c266870
ira-2035 <113875890+ira-2035@users.noreply.github.com>
2024-06-03 06:15:35 +0000

    set as homepage deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ira Soni",
+  title: "Fresher",
+  email: "soniira43@gmail.com",
+  gitHub: "ira-2035",
+  instagram: "irasoni10",
+  linkedIn: "Ira Soni",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
